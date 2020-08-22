66dce6a156cb729cb0eae5948e466a213bdac2d3
busgandy <39296111+busgandy@users.noreply.github.com>
2020-08-22 22:47:44 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,11 +6,3 @@ app = Flask(__name__)
 def index():
     return render_template ("index.html")
 
-@app.route("/analysis")
-def index():
-    return render_template ("analysis.html")
-
-@app.route("/Start Predictions")
-def index():
-    return render_template ("Start Predictions.html")
-
