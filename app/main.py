1e3d281da8245128356f04aa089b2f525b7904de
busgandy <39296111+busgandy@users.noreply.github.com>
2020-08-22 23:40:22 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,4 +9,9 @@ def index():
 @app.route("/analysis", methods = ["GET", "POST"])
 def analysis():
     return render_template ("analysis.html")
-
+def contact():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'START YOUR PREDICTION':
+            return render_template ("analysis.html")
+        elif request.form['submit_button'] == 'CHECK FOR ANALYSIS':
+            return render_template ("Start Predictions.html")
