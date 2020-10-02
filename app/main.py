e197a1a99e957f279cd83a142357aeb825c4c7ae
busgandy <39296111+busgandy@users.noreply.github.com>
2020-10-03 00:26:27 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, render_template, request
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open("linear_regression.pkl", "rb"))
+
 
 @app.route("/")
 def index():
@@ -10,8 +13,20 @@ def index():
 def analysis():
     return render_template ("analysis.html")
 @app.route("/prediction")
-def prediction():
+def prediction():    
     return render_template ("Start Predictions.html")
+    
+@app.route("/predict", methods=["POST"])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    output = round(prrediction[0], 2)
+    return render_template ( "Start Predictions.html" , prediction_text = "Employee Salary is: ${}".format(output))
+
+
+
 def contact():
     if request.method == 'POST':
         if request.form['submit_button'] == 'START YOUR PREDICTION':
@@ -19,6 +34,10 @@ def contact():
         elif request.form['submit_button'] == 'CHECK FOR ANALYSIS':
             return render_template ("Start Predictions.html")
 
+
+        
+
+        
 @app.route("/start", methods = ["GET", "POST"])
 def start_prediction():
     return render_template ("Start Predictions.html")
